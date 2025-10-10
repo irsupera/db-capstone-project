7c4430979a04bbc5ced9ec2be157102f9eb42391
irsupera <97858090+irsupera@users.noreply.github.com>
2025-10-10 02:00:40 -0300

    Adding stored procedures - manage booking

---
+++ ManageBooking.sql
@@ -0,0 +1,28 @@
+DELIMITER //
+CREATE PROCEDURE ManageBooking(IN bdate DATETIME, IN tnum INT)
+BEGIN
+DECLARE no_entries INT;
+SELECT 
+    COUNT(*)
+INTO no_entries FROM
+    Bookings
+WHERE
+    BookingSlot = bdate
+        AND TableNumber = tnum;
+START TRANSACTION;
+INSERT INTO Bookings (TableNumber, BookingSlot) VALUES (tnum,bdate);
+IF no_entries > 0 THEN
+	ROLLBACK;
+SELECT 
+    CONCAT('Table ',
+            tnum,
+            ' is already booked - booking cancelled.') AS 'Booking Status';
+ELSE 
+	COMMIT;
+SELECT 
+    CONCAT('Table ',
+            tnum,
+            ' has been successfully booked') AS 'Booking Status';
+END IF;
+END //
+DELIMITER ;
